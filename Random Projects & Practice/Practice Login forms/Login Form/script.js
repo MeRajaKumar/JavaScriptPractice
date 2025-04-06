fb99c0b0b7294a141c4f34bd938fdfeedcb8e191
Raja Kumar <teamaayushgupta@gmail.com>
2025-04-06 19:25:48 +0530

    initial commit today

---
+++ Random Projects & Practice/Practice Login forms/Login Form/script.js
@@ -0,0 +1,10 @@
+var innerbox = document.getElementById("innerbox");
+    
+    function openRegister(){
+    innerbox.style.transform = "rotateY(-180deg)";
+    var num=3;
+    console.log(num);
+    }
+    function openLogin(){
+    innerbox.style.transform = "rotateY(0deg)";
+    }
